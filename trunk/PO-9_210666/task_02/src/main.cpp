2956fbf5b235540c1d0da14d6af4a89fccc8fb2d
HeyJIOBUM <100068694+HeyJIOBUM@users.noreply.github.com>
2023-10-20 23:09:05 +0300

    Update main.cpp

--- trunk/PO-9_210666/task_02/src/main.cpp
+++ trunk/PO-9_210666/task_02/src/main.cpp
@@ -4,11 +4,14 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
+    QString 
     app.setOrganizationDomain("example.com");
     app.setOrganizationName("ExampleSoft");
+    QString ochenPolesnayaPeremennaya;
+    //kak же silno ya lubly proverky na code duplication 
     app.setWindowIcon(QIcon(":/icons/images/new.png"));
     app.setApplicationVersion("0.1");
+    QString ocherednayaPolesnayaPeremennaya;
     app.setApplicationName("Text Editor");
 
     TextEditor txtEdit;
