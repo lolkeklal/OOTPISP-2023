42082cf4cac7fa49e8e8f7845ef414283e53654e
dima <dmitriy.solyshko@mail.ru>
2023-11-10 09:58:22 +0300

    fix

--- trunk/PO-9_210668/task_01/src/main.cpp
+++ trunk/PO-9_210668/task_01/src/main.cpp
@@ -1,11 +1,11 @@
-#include "mainwindow.h"
-
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
-}
+#include "mainwindow.h"// 210668_Подключаем библиотеку "mainwindow.h"
+//210668
+#include <QApplication>// 210668_Подключаем библиотеку QApplication
+//210668
+int main(int argc, char *argv[])// 210668
+{ // 210668
+    QApplication a(argc, argv);// 210668
+    MainWindow w; // 210668
+    w.show(); // 210668
+    return a.exec(); // 210668
+} // 210668
