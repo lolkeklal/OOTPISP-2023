148550179a9e00f78f05d11b67f1bb7a0acd43a5
Darya Shubich <99385925+Dashubaa@users.noreply.github.com>
2023-11-20 10:13:24 +0300

    [task_02] Add lab2 (#34)
    * Create trunc
    
    * Delete trunc
    
    * laba1
    
    * Update README.md
    
    * Update Readme.md
    
    * Update Readme.md
    
    * add report for laba2
    
    * add code files
    
    * add readme
    
    * Add lab2
    
    * add lab2
    
    * add laba2
    
    * change img names, removespaces
    
    * Update README.md
    
    * change folder name
    
    * add cmake
    
    * upd readme, cmakelist
    
    * hz
    
    * Update texteditor.h
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Дарья Шубич <dashubaa@MacBook-Pro-Dara.local>
    Co-authored-by: redCat <yana.sitkowecz@yandex.ru>

---
+++ trunk/PO-9_210674/task_02/src/main.cpp
@@ -0,0 +1,20 @@
+#include "texteditor.h"
+
+#include <QApplication>
+#include <QIcon>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    a.setApplicationName("Text Editor");
+    a.setApplicationVersion("0.1");
+
+
+    a.setOrganizationName("ExampleSoft");
+    a.setOrganizationDomain("example.com");
+    QIcon icon(":/Users/dashubaa/univer/3kurs/oop/OOTPISP-2023/tasks/task_02/images/tables/t1_1.png");
+    a.setWindowIcon(icon);
+   TextEditor w("example.txt");
+    w.show();
+    return a.exec();
+}
