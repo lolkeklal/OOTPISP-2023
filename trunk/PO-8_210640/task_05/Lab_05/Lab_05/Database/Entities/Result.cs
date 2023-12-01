434724d72768dad91901a45dc2b1837126cadc75
Антон Лобарев <88208173+ToxaChiter@users.noreply.github.com>
2023-12-01 16:55:48 +0300

    [task_05] Lab 05 (#155)
    * Add Lab 05
    
    * Fix codacy issues

---
+++ trunk/PO-8_210640/task_05/Lab_05/Lab_05/Database/Entities/Result.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+
+namespace Lab_05.Database.Entities;
+
+public class Result : INotifyPropertyChanged
+{
+    public int Id { get; set; }
+
+    private Student _student;
+    public virtual Student Student
+    {
+        get => _student;
+        set
+        {
+            if (_student == value)
+            {
+                return;
+            }
+
+            _student = value;
+            PropertyChanged?.Invoke(this, new(nameof(Student)));
+        }
+    }
+
+    private Session _session;
+    public virtual Session Session
+    {
+        get => _session;
+        set
+        {
+            if (_session == value)
+            {
+                return;
+            }
+
+            _session = value;
+            PropertyChanged?.Invoke(this, new(nameof(Session)));
+        }
+    }
+
+    private bool _passed;
+    public bool Passed
+    {
+        get => _passed;
+        set
+        {
+            if (_passed == value) 
+            {
+                return;
+            }
+
+            _passed = value;
+            PropertyChanged?.Invoke(this, new(nameof(Passed)));
+        }
+    }
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public override string ToString()
+    {
+        return $"{Student} {Session} {(Passed ? "Passed" : "Failed")}";
+    }
+}
