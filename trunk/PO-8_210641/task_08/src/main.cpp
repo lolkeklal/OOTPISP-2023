bb5613bd96d0cefdd182f65c7adf6a1b0b37be86
Makarevich Ekaterina <99526931+Mkrshkk@users.noreply.github.com>
2023-12-13 21:23:50 +0300

    [task_08] Add task_08 (#283)
    * Add lab8
    
    * Fix
    
    * Fix CMakeLists.txt
    
    * Fix cmake
    
    ---------
    
    Co-authored-by: Mkrshkk <Mkrshkk@users.noreply.github.com>

---
+++ trunk/PO-8_210641/task_08/src/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+#include "AbstractFactory.h"
+
+int main() {
+    AbstractFactory* winFactory = new WindowsFactory();
+
+    List* list = winFactory->createList();
+    InputField* inputField = winFactory->createInputField();
+    Button* button = winFactory->createButton();
+    Language* language = winFactory->createLanguage();
+
+    list->render();
+    inputField->render();
+    button->render();
+    language->display();
+
+    delete winFactory;
+    delete list;
+    delete inputField;
+    delete button;
+    delete language;
+}
