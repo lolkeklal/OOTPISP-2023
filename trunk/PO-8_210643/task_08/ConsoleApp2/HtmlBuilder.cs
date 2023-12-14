a491d5eb0febfbe7aae63c25840e0266a6324688
AlexaBailey <alexaserko@gmail.com>
2023-12-14 08:58:37 +0300

    [task_08]Add task_08 (#184)
    * Add task_08
    
    * Add task_08
    
    * Fixed task_08 README.md and task_07 README made the same as in task_07 branch
    
    * Add task_08
    
    * Add task_08
    
    * Fixed task_08 README.md and task_07 README made the same as in task_07 branch
    
    * Add task_08
    
    ---------
    
    Co-authored-by: Yana Sitkovets <yana.sitkowecz@yandex.ru>

---
+++ trunk/PO-8_210643/task_08/ConsoleApp2/HtmlBuilder.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1;
+
+public class HtmlBuilder : Builder
+{
+    private Product _product = new Product();
+    public override void BuildHeader()
+    {
+        _product.AddHeader("<");
+    }
+
+    public override void BuildBlock()
+    {
+        _product.AddBlock("h");
+    }
+
+    public override void BuildEnding()
+    {
+        _product.AddEnding(">");
+    }
+
+    public override Product GetResult()
+    {
+        return _product;
+    }
+}
---
+++ trunk/PO-8_210643/task_08/ConsoleApp2/XlsBuilder.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1;
+
+public class XlsBuilder : Builder
+{
+    private Product _product = new Product();
+    public override void BuildHeader()
+    {
+        _product.AddHeader("Table");
+    }
+
+    public override void BuildBlock()
+    {
+        _product.AddBlock("data");
+    }
+
+    public override void BuildEnding()
+    {
+        _product.AddEnding("Created by Sasha");
+    }
+
+    public override Product GetResult()
+    {
+        return _product;
+    }
+}
