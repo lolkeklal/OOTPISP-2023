3d1517fcd76d77e63d69300caf0a32799f3a85a7
Андрей Шлыков <99527318+HarWeasttt@users.noreply.github.com>
2023-12-15 09:38:25 +0300

    [task_07] add task_07 (#267)
    Co-authored-by: Yana Sitkovets <yana.sitkowecz@yandex.ru>

---
+++ trunk/PO-8_210648/task_07/ConsoleApp1/ConsoleApp1/AbstractFabric.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApp1;
+
+public class AbstractFabric
+{
+    public virtual Door CreateDoor(Room r1, Room r2)
+    {
+        return new Door(r1, r2);
+    }
+
+    public virtual Room CreateRoom(int n)
+    {
+        return new Room(n);
+    }
+    
+}
+
+public class PrototypeFabric : AbstractFabric
+{
+    private Room _room;
+    private Door _door;
+
+
+    public PrototypeFabric(Room room, Door door)
+    {
+        _room = room;
+        _door = door;
+    }
+
+    public override Door CreateDoor(Room r1, Room r2)
+    {
+        return _door.Clone();
+    }
+
+    public override Room CreateRoom(int n)
+    {
+        return _room.Clone();
+    }
+}
+
+public class FabricArmored : AbstractFabric
+{
+    public override Door CreateDoor(Room r1, Room r2)
+    {
+        return new ArmoredDoor(r1, r2, 10);
+    }
+
+    public override Room CreateRoom(int n)
+    {
+        return new ArmoredRoom(n, 10);
+    }
+}
---
+++ trunk/PO-8_210648/task_07/ConsoleApp1/ConsoleApp2/Director.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp2;
+
+public class Director
+{
+    private Builder _bilder;
+
+    public Director(Builder builder)
+    {
+        _bilder = builder;
+    }
+
+    public Product Construct()
+    {
+        _bilder.BuildEyes();
+        _bilder.BuildNose();
+        _bilder.BuildMouth();
+        _bilder.BuildEars();
+        _bilder.BuildHair();
+        return _bilder.GetResult();
+    }
+}
