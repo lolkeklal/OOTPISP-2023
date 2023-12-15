09f997c4ab2f5db0cfd362dd237bb9770973e738
Андрей Шлыков <99527318+HarWeasttt@users.noreply.github.com>
2023-12-15 08:59:03 +0300

    [task_03] add task_03 (#224)
    * [task_03] add task_03
    
    * Update README.md
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Yana Sitkovets <yana.sitkowecz@yandex.ru>

---
+++ trunk/PO-8_210648/task_03/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Task1_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowT1 windowT1 = new WindowT1();
+            windowT1.Show();
+        }
+
+        private void Task2_OnClick(object sender, RoutedEventArgs e)
+        {
+            Window2Task window2Task = new Window2Task();
+            window2Task.Show();
+        }
+
+        private void Task3_OnClick(object sender, RoutedEventArgs e)
+        {
+            Window3Task window3Task = new Window3Task();
+            window3Task.Show();
+        }
+
+        private void Task4_OnClick(object sender, RoutedEventArgs e)
+        {
+            Window4Task window4Task = new Window4Task();
+            window4Task.Show();
+        }
+
+        private void Task5_OnClick(object sender, RoutedEventArgs e)
+        {
+            Window5Task window5Task = new Window5Task();
+            window5Task.Show();
+        }
+
+        private void Task6_OnClick(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine($"{Math.Sqrt(3) % 1}");
+            Window6Task window6Task = new Window6Task();
+            window6Task.Show();
+        }
+    }
+}
---
+++ trunk/PO-8_210648/task_03/WpfApp1/Window6Task.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace WpfApp1;
+
+public partial class Window6Task : Window
+{
+    public Window6Task()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonA_OnClick(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.ShowDialog();
+        string text;
+        using (StreamReader streamReader = new StreamReader(openFileDialog.FileName))
+        {
+            text =  streamReader.ReadToEnd();
+        }
+
+        string[] inputs = text.Split(',');
+        List<int> list = new List<int>();
+        foreach (var input in inputs)
+        {
+            list.Add(Convert.ToInt32(input));
+        }
+
+        string res = "";
+        foreach (var item in list)
+        {
+            if (item % 2 == 0)
+            {
+                res += $"{item},";
+            }
+        }
+
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+        saveFileDialog.ShowDialog();
+        using (StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName))
+        {
+            streamWriter.WriteLineAsync(res);
+        }
+    }
+
+    private void ButtonB_OnClick(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.ShowDialog();
+        string text;
+        using (StreamReader streamReader = new StreamReader(openFileDialog.FileName))
+        {
+            text =  streamReader.ReadToEnd();
+        }
+
+        string[] inputs = text.Split(',');
+        List<int> list = new List<int>();
+        foreach (var input in inputs)
+        {
+            list.Add(Convert.ToInt32(input));
+        }
+
+        string res = "";
+        foreach (var item in list)
+        {
+            if (item % 3 == 0 && item % 7 != 0)
+            {
+                res += $"{item},";
+            }
+        }
+
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+        saveFileDialog.ShowDialog();
+        using (StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName))
+        {
+            streamWriter.WriteLineAsync(res);
+        }
+    }
+
+    private void ButtonC_OnClick(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.ShowDialog();
+        string text;
+        using (StreamReader streamReader = new StreamReader(openFileDialog.FileName))
+        {
+            text =  streamReader.ReadToEnd();
+        }
+
+        string[] inputs = text.Split(',');
+        List<int> list = new List<int>();
+        foreach (var input in inputs)
+        {
+            list.Add(Convert.ToInt32(input));
+        }
+
+        string res = "";
+        foreach (var item in list)
+        {
+            if (Math.Sqrt(item) % 1 == 0)
+            {
+                res += $"{item},";
+            }
+        }
+
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+        saveFileDialog.ShowDialog();
+        using (StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName))
+        {
+            streamWriter.WriteLineAsync(res);
+        }
+    }
+
+   
+}
